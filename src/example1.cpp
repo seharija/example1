e5205dee7ff372cec10453aa44ee467324f389d7
srija <sehar.rija@ryerson.ca>
2015-10-26 16:48:57 -0400

    added a new line

--- src/example1.cpp
+++ src/example1.cpp
@@ -13,5 +13,6 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "My name is Sehar" << endl;
+	cout << "My last name is Rija" << endl;
 	return 0;
 }
